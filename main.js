a3210e02684adb0a51a85f2c741954812e4ab3ee
Thabang-Magaele <magaelethabang@gmail.com>
2026-02-18 09:48:00 +0200

    update

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Make sure initMap is global so the Google script can find it
 window.initMap = async function() {
-  const location = "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3652.9653522385725!2d31.099743!3d-25.337376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjXCsDIwJzE0LjYiUyAzMcKwMDUnNTkuMSJF!5e1!3m2!1sen!2sza!4v1771400334167!5m2!1sen!2sza";
+  const location = { lat: 50, lng: 50 };
 
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 17,
+    zoom: 10,
     center: location,
   });
 
